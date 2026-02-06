a49a8252182d06ff3e21e8aaae9968660cd558cb
renatobrunetto <63797102+renatobrunetto@users.noreply.github.com>
2026-02-06 16:48:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// Firebase imports (CDN)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { 
-  getAuth, 
-  signInWithPopup, 
-  GoogleAuthProvider, 
-  onAuthStateChanged 
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// 🔑 CONFIG
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAEX1MOFqLp1UDO8SfN4oMqDQx_8NhEH8w",
   authDomain: "cs2-strategy.firebaseapp.com",
@@ -20,26 +16,17 @@ const firebaseConfig = {
   measurementId: "G-V7FZK2FX3J"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-};
-
-// Init
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// UI
 const loginBtn = document.getElementById("loginBtn");
 const status = document.getElementById("status");
 
-// Login
-loginBtn.onclick = async () => {
+loginBtn.addEventListener("click", async () => {
   await signInWithPopup(auth, provider);
-};
+});
 
-// State
 onAuthStateChanged(auth, (user) => {
   if (user) {
     status.textContent = `logado como ${user.displayName}`;
